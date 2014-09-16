2dad7776a73ce0b154202e167128fb627a9fb920
Bernardo <mafagafogigante@gmail.com>
2014-09-16 12:54:16 -0300

    TESTs

---
+++ src/org/dungeon/core/creatures/CreaturePreset.java
@@ -0,0 +1,62 @@
+package org.dungeon.core.creatures;
+
+/**
+ * An enumerated type of presets for creature creation.
+ * Created by Bernardo Sulzbach on 16/09/14.
+ */
+public enum CreaturePreset {
+
+    BAT(CreatureType.BEAST, CreatureID.BAT, 12, 4, 5, 2, 15);
+
+    private final CreatureType type;
+    private final CreatureID id;
+    private final int health;
+    private final int healthIncrement;
+    private final int attack;
+    private final int attackIncrement;
+    private final int experienceDropFactor;
+
+    CreaturePreset(CreatureType type,
+                   CreatureID id,
+                   int health,
+                   int healthIncrement,
+                   int attack,
+                   int attackIncrement,
+                   int experienceDropFactor) {
+        this.type = type;
+        this.id = id;
+        this.health = health;
+        this.healthIncrement = healthIncrement;
+        this.attack = attack;
+        this.attackIncrement = attackIncrement;
+        this.experienceDropFactor = experienceDropFactor;
+    }
+
+    public CreatureType getType() {
+        return type;
+    }
+
+    public CreatureID getId() {
+        return id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getHealthIncrement() {
+        return healthIncrement;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getAttackIncrement() {
+        return attackIncrement;
+    }
+
+    public int getExperienceDropFactor() {
+        return experienceDropFactor;
+    }
+}
