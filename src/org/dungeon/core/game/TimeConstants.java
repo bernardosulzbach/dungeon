0c80a3f4e0ebd7bb3964026f47eed9602701eac7
mafagafogigante <mafagafogigante@gmail.com>
2014-09-25 22:16:53 -0300

    Started a TimeConstants helper class.

---
+++ src/org/dungeon/core/game/TimeConstants.java
@@ -0,0 +1,15 @@
+package org.dungeon.core.game;
+
+/**
+ * The constants that determine the time flow of the game.
+ *
+ * Created by Bernardo on 25/09/2014.
+ */
+public class TimeConstants {
+
+    // How many seconds the player needs to cross a location.
+    public static final int WALK_SUCCESS = 200;
+    // How many seconds the player wastes when he walks into a blocked path.
+    public static final int WALK_BLOCKED = 5;
+
+}
