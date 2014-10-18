694a34a637688ef477ad8a3c64e63d776d9be1e2
mafagafogigante <mafagafogigante@gmail.com>
2014-10-18 16:55:37 -0300

    Refactored licenses.

--- src/org/dungeon/core/items/ItemPreset.java
+++ src/org/dungeon/core/items/ItemPreset.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright (C) 2014 Bernardo Sulzbach
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.dungeon.core.items;
 
 public enum ItemPreset {
