763cbf3b9ba7195f87dc300c32a8b3acb34a7811
mafagafo <mafagafogigante@gmail.com>
2014-12-16 11:49:10 -0200

    Implements DResourceReader.
    A good abstraction to the process of opening a text file and making blueprints.
    
    Better yet, this commit removes repeated code.

---
+++ src/main/java/org/dungeon/game/DResourceReader.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Bernardo Sulzbach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.dungeon.game;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+/**
+ * DResourceReader class that eases the parsing of resource files.
+ * <p/>
+ * Created by Bernardo Sulzbach on 16/12/14.
+ */
+public class DResourceReader implements Closeable {
+
+  private final HashMap<String, String> map;
+  private DBufferedReader dBufferedReader;
+
+  public DResourceReader(InputStream inputStream) {
+    map = new HashMap<String, String>();
+    dBufferedReader = new DBufferedReader(new InputStreamReader(inputStream));
+  }
+
+  public String getValue(String identifier) {
+    return map.get(identifier);
+  }
+
+  /**
+   * Reads the next element in the resource file to memory.
+   *
+   * @return true if a new element was read; false otherwise.
+   */
+  public boolean readNextElement() {
+    map.clear();
+    boolean readNewValue = true;
+    while (readNewValue) {
+      readNewValue = readNextValue();
+    }
+    return !map.isEmpty();
+  }
+
+  /**
+   * Reads the next ID: value pair from the BufferedReader.
+   *
+   * Does not put a new value in the HashMap if the HashMap already contains a key that matches the value's ID.
+   *
+   * @return true if a new value was read; false otherwise.
+   */
+  private boolean readNextValue() {
+    String string = dBufferedReader.readString();
+    if (string != null && !string.isEmpty()) {
+      int colonIndex = string.indexOf(':');
+      String valueId = string.substring(0, colonIndex).trim();
+      if (!map.containsKey(valueId)) {
+        map.put(valueId, string.substring(colonIndex + 1).trim());
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void close() throws IOException {
+    dBufferedReader.close();
+  }
+
+}
--- src/main/java/org/dungeon/game/GameData.java
+++ src/main/java/org/dungeon/game/GameData.java
@@ -128,38 +128,19 @@ public final class GameData {
 
   private static void loadCreatureBlueprints() {
     CREATURE_BLUEPRINTS = new HashMap<ID, CreatureBlueprint>();
-    BufferedReader br = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("creatures.txt")));
-    String line;
-    CreatureBlueprint blueprint = new CreatureBlueprint();
-    try {
-      while ((line = br.readLine()) != null) {
-        if (Utils.isNotBlankString(line)) {
-          if (line.startsWith("ID:")) {
-            if (blueprint.getId() != null) {
-              CREATURE_BLUEPRINTS.put(blueprint.getId(), blueprint);
-              blueprint = new CreatureBlueprint();
-            }
-            blueprint.setId(new ID(Utils.getAfterColon(line).trim()));
-          } else if (line.startsWith("TYPE:")) {
-            blueprint.setType(Utils.getAfterColon(line).trim());
-          } else if (line.startsWith("NAME:")) {
-            blueprint.setName(Utils.getAfterColon(line).trim());
-          } else if (line.startsWith("CUR_HEALTH:")) {
-            blueprint.setCurHealth(Integer.parseInt(Utils.getAfterColon(line).trim()));
-          } else if (line.startsWith("MAX_HEALTH:")) {
-            blueprint.setMaxHealth(Integer.parseInt(Utils.getAfterColon(line).trim()));
-          } else if (line.startsWith("ATTACK:")) {
-            blueprint.setAttack(Integer.parseInt(Utils.getAfterColon(line).trim()));
-          } else if (line.startsWith("ATTACK_ALGORITHM_ID:")) {
-            blueprint.setAttackAlgorithmID(Utils.getAfterColon(line).trim());
-          }
-        }
-      }
-      br.close();
-    } catch (IOException exception) {
-      DLogger.warning(exception.toString());
+    DResourceReader resourceReader = new DResourceReader(loader.getResourceAsStream("creatures.txt"));
+    CreatureBlueprint blueprint;
+    while (resourceReader.readNextElement()) {
+      blueprint = new CreatureBlueprint();
+      blueprint.setId(new ID(resourceReader.getValue("ID")));
+      blueprint.setType(resourceReader.getValue("TYPE"));
+      blueprint.setName(resourceReader.getValue("NAME"));
+      blueprint.setCurHealth(Integer.parseInt(resourceReader.getValue("CUR_HEALTH")));
+      blueprint.setMaxHealth(Integer.parseInt(resourceReader.getValue("MAX_HEALTH")));
+      blueprint.setAttack(Integer.parseInt(resourceReader.getValue("ATTACK")));
+      blueprint.setAttackAlgorithmID(resourceReader.getValue("ATTACK_ALGORITHM_ID"));
+      CREATURE_BLUEPRINTS.put(blueprint.getId(), blueprint);
     }
-    CREATURE_BLUEPRINTS.put(blueprint.getId(), blueprint);
     DLogger.info("Loaded " + CREATURE_BLUEPRINTS.size() + " creature blueprints.");
   }
 
