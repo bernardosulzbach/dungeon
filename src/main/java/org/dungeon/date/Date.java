2959e3dc7a293d347461cb62fcdef2aad6b16ec7
mafagafo <mafagafogigante@gmail.com>
2014-12-16 17:13:23 -0200

    Better validation in Date constructor.

--- src/main/java/org/dungeon/date/Date.java
+++ src/main/java/org/dungeon/date/Date.java
@@ -30,12 +30,12 @@ import java.io.Serializable;
  */
 public class Date implements Serializable {
 
-  private final long time;
-  private static final long daysInMonth = 10;
-  private static final long monthsInYear = 10;
+  public static final long DAYS_IN_MONTH = 10;
+  public static final long MONTHS_IN_YEAR = 10;
   private static final long millisInDay = 1000 * 60 * 60 * 24;
-  private static final long millisInMonth = millisInDay * daysInMonth;
-  private static final long millisInYear = millisInMonth * monthsInYear;
+  private static final long millisInMonth = millisInDay * DAYS_IN_MONTH;
+  private static final long millisInYear = millisInMonth * MONTHS_IN_YEAR;
+  private final long time;
 
   private Date() {
     time = 0;
@@ -45,7 +45,7 @@ public class Date implements Serializable {
     time = millis;
   }
 
-  public Date(int year, int month, int day) {
+  public Date(long year, long month, long day) {
     if (year < 0) {
       DLogger.warning("Tried to construct Date with negative year!");
       year = 0;
@@ -53,10 +53,16 @@ public class Date implements Serializable {
     if (month < 0) {
       DLogger.warning("Tried to construct Date with negative month!");
       month = 0;
+    } else if (month > MONTHS_IN_YEAR) {
+      DLogger.warning("Tried to construct Date with nonexistent month.");
+      month = MONTHS_IN_YEAR;
     }
     if (day < 0) {
       DLogger.warning("Tried to construct Date with negative day!");
       day = 0;
+    } else if (day > DAYS_IN_MONTH) {
+      DLogger.warning("Tried to construct Date with nonexistent day.");
+      day = DAYS_IN_MONTH;
     }
     time = millisInYear * year + millisInMonth * month + millisInDay * day;
   }
