6395c167742875159d93f9b8e7717e5333296a65
mafagafogigante <mafagafogigante@gmail.com>
2015-02-06 14:34:44 -0200

    Wiki draft.

---
+++ src/main/java/org/dungeon/wiki/Wiki.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 Bernardo Sulzbach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.dungeon.wiki;
+
+import org.dungeon.io.IO;
+
+/**
+ * The Wiki class. Loads the contents of the wiki.txt file and manages wiki articles.
+ *
+ * Created by Bernardo on 06/02/2015.
+ */
+public class Wiki {
+
+  /**
+   * Searches the wiki and prints the matching contents to the screen.
+   *
+   * @param searchArguments the arguments of the issued command
+   */
+  public void search(String searchArguments) {
+    IO.writeString("Wiki not yet implemented.");
+  }
+
+}
