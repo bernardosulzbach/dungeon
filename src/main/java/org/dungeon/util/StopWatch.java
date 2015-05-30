125cbef62d8c8facc4b5ce29a4f0d1e08fab1f3d
mafagafo <mafagafogigante@gmail.com>
2015-05-30 17:24:42 -0300

    Remove magic number from StopWatch.

--- src/main/java/org/dungeon/util/StopWatch.java
+++ src/main/java/org/dungeon/util/StopWatch.java
@@ -26,6 +26,7 @@ package org.dungeon.util;
  */
 public class StopWatch {
 
+  private static final int MILLISECOND_IN_NANOSECONDS = 1000000;
   private final long time;
 
   public StopWatch() {
@@ -39,7 +40,7 @@ public class StopWatch {
    */
   public String toString() {
     final long difference = System.nanoTime() - time;
-    return String.format("%d ms", difference / 1000000);
+    return String.format("%d ms", difference / MILLISECOND_IN_NANOSECONDS);
   }
 
 }
