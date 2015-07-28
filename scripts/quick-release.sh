27b5f1e126351658be7357b9d4075fe5d84d1c50
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-07-28 12:59:11 -0300

    quick-release no longer has to update the readme.

--- scripts/quick-release.sh
+++ scripts/quick-release.sh
@@ -3,7 +3,7 @@ if [ $# -ne 2 ]
         echo "Pass two arguments: the new tag and a short release message."
     else
         old_tag=$(git describe --abbrev=0)
-        sed -i "s/$old_tag/$1/g" README.md pom.xml
+        sed -i "s/$old_tag/$1/g" pom.xml
         git commit -a -m "Release $1."
         git tag -a $1 -m "$2"
         echo "Check if everything is OK and if it is push to the remote."
