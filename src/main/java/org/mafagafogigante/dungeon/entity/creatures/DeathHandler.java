292aa2b5febc66e72020efb8518f555d60513b74
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-12-19 20:29:04 -0200

    Removed unnecessary null check

--- src/main/java/org/mafagafogigante/dungeon/entity/creatures/DeathHandler.java
+++ src/main/java/org/mafagafogigante/dungeon/entity/creatures/DeathHandler.java
@@ -38,11 +38,7 @@ final class DeathHandler {
     defeatedLocation.removeCreature(creature);
     if (creature.hasTag(Creature.Tag.CORPSE)) {
       Item item = ItemFactory.makeCorpse(creature, defeatedLocation.getWorld().getWorldDate());
-      if (item != null) {
-        defeatedLocation.addItem(item);
-      } else {
-        DungeonLogger.warning(creature.getName() + " has the CORPSE tag, but no corpse preset was found.");
-      }
+      defeatedLocation.addItem(item);
     }
     creature.getDropper().dropEverything();
     DungeonLogger.fine("Disposed of " + creature.getName() + " at " + creature.getLocation() + ".");
