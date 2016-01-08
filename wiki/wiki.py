51bb90b530196a31b8dae8ab672e1c25b29edefc
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-01-08 13:39:04 -0200

    Wiki script also producing See Also section

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -11,6 +11,17 @@ def make_filename_for_article(title):
     return title + '.md'
 
 
+def make_markdown_for_article(article):
+    title = article['title']
+    markdown = ['# ' + title, '\n\n']
+    markdown.extend(article['content'])
+    see_also = article.get('seeAlso')
+    if see_also is not None:
+        markdown.append('\n\n')
+        markdown.append('## See Also' + '\n\n' + '; '.join(see_also))
+    return markdown
+
+
 if __name__ == '__main__':
     dungeon_root = os.path.dirname(os.path.dirname(__file__))
     wiki_json_path = os.path.join(dungeon_root, 'src', 'main', 'resources', 'wiki.json')
@@ -18,8 +29,7 @@ if __name__ == '__main__':
         wiki_json = json.load(wiki_file)
         for article in wiki_json['articles']:
             article_title = article['title']
-            markdown = ['# ' + article_title + '\n\n']
-            markdown.extend(article['content'])
+            markdown = make_markdown_for_article(article)
             file_path = os.path.join(dungeon_root, 'dungeon.wiki', make_filename_for_article(article_title))
             with open(file_path, 'w') as article_file:
                 article_file.writelines(markdown)
