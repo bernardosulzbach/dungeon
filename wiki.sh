4bfc1cdf3c47ed8023e3f15129000d486e3f6946
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-01-21 03:02:56 -0200

    Should have proper documentation for wiki script
    Closes #208

--- wiki.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python3 wiki/wiki.py
-cd dungeon.wiki
-git commit -am "Regenerated the Wiki"
-git push origin
---
+++ wiki/wiki.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Delete the wiki clone if it is there (shouldn't be)
+rm -rf dungeon.wiki
+
+# Clone the wiki
+git clone git@github.com:mafagafogigante/dungeon.wiki.git
+
+# Run the converter
+python3 wiki/wiki.py
+
+# Commit changes and update remote
+cd dungeon.wiki
+git commit -am "Regenerated the Wiki"
+git push origin
+
+# Self-destruct
+cd ..
+rm -rf dungeon.wiki
