76fdf1bcc82e250e86ca6ffcfbde7780cd81fd16
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-12-12 00:11:49 -0200

    Refactored IdTest to use JUnit expected exceptions

--- src/test/java/org/mafagafogigante/dungeon/game/IdTest.java
+++ src/test/java/org/mafagafogigante/dungeon/game/IdTest.java
@@ -1,31 +1,29 @@
 package org.mafagafogigante.dungeon.game;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class IdTest {
 
+  @Test(expected = IllegalArgumentException.class)
+  public void constructorShouldThrowExceptionOnNull() throws Exception {
+    new Id(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructorShouldThrowExceptionOnEmptyString() throws Exception {
+    new Id("");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructorShouldThrowExceptionOnStringWithSpaces() throws Exception {
+    new Id(" ");
+  }
+
   @Test
-  public void testConstructor() throws Exception {
-    try {
-      new Id(null);
-      Assert.fail("expected an IllegalArgumentException");
-    } catch (IllegalArgumentException expected) {
-    }
-    try {
-      new Id("");
-      Assert.fail("expected an IllegalArgumentException");
-    } catch (IllegalArgumentException expected) {
-    }
-    try {
-      new Id(" ");
-      Assert.fail("expected an IllegalArgumentException");
-    } catch (IllegalArgumentException expected) {
-    }
+  public void constructorShouldWorkForValidStrings() throws Exception {
     new Id("403_ACCESS_FORBIDDEN");
     new Id("404_NOT_FOUND");
     new Id("VALID_ID");
-    new Id("YET_ANOTHER_VALID_ID");
     new Id("ID_WITH_NUMBER_255");
   }
 
