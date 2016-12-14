5377e89336e1809ca1e172d8c1920693a692785a
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-12-14 02:27:24 -0200

    Should set application title on Gnome
    This is done via reflection by setting awtAppClassName.
    
    Closes #307

--- src/main/java/org/mafagafogigante/dungeon/gui/GameWindow.java
+++ src/main/java/org/mafagafogigante/dungeon/gui/GameWindow.java
@@ -19,6 +19,7 @@ import java.awt.FontMetrics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
@@ -28,6 +29,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
@@ -157,7 +159,7 @@ public class GameWindow extends JFrame {
     constants.insets = new Insets(0, MARGIN, MARGIN, MARGIN);
     panel.add(textField, constants);
 
-    setTitle(WINDOW_TITLE);
+    setGameWindowTitle(WINDOW_TITLE);
 
     setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
     addWindowListener(new ClosingListener());
@@ -229,6 +231,27 @@ public class GameWindow extends JFrame {
 
     setResizable(false);
     resize();
+  }
+
+  private void setGameWindowTitle(String title) {
+    setTitle(title);
+    // Set the ClassName so that Gnome presents the window title correctly.
+    // See https://github.com/mafagafogigante/dungeon/issues/307 for more information.
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Field awtAppClassNameField = null;
+    try {
+      awtAppClassNameField = toolkit.getClass().getDeclaredField("awtAppClassName");
+    } catch (NoSuchFieldException logged) {
+      DungeonLogger.logSevere(logged);
+    }
+    if (awtAppClassNameField != null) {
+      awtAppClassNameField.setAccessible(true);
+      try {
+        awtAppClassNameField.set(toolkit, title);
+      } catch (IllegalAccessException logged) {
+        DungeonLogger.logSevere(logged);
+      }
+    }
   }
 
   /**
