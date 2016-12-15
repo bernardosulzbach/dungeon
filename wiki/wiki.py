1e4f4106887d93474117630584b58f1763c19944
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-12-15 01:46:52 -0200

    Should include Dungeon version in Wiki
    Also removed unnecessary close() calls.

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -2,10 +2,25 @@
 
 import json
 import os
+import xml.etree.ElementTree as ElementTree
+
+
+def get_version_string():
+    tree = ElementTree.parse('pom.xml')
+    root = tree.getroot()
+    for child in root:
+        # Check the end of the tag to ignore the namespace Maven uses.
+        if child.tag.endswith('version'):
+            return child.text
+    raise Exception('Failed to find the repository version!')
 
 
 def get_wiki_root_url():
     return 'https://github.com/mafagafogigante/dungeon/wiki/'
+
+
+def get_footer_filename():
+    return '_Footer.md'
 
 
 def make_filename_for_article(title):
@@ -50,19 +65,41 @@ def clean_wiki_clone(dungeon_root):
             os.remove(os.path.join(root, filename))
 
 
+def get_home_markdown():
+    lines = ['# Welcome', '\n\n', 'Welcome to the Dungeon wiki!', '\n\n', '# About the wiki', '\n\n']
+    wiki_json_file = 'https://github.com/mafagafogigante/dungeon/blob/master/src/main/resources/wiki.json'
+    lines.append('This wiki is automatically generated from [this JSON file]({}).'.format(wiki_json_file))
+    lines.append('\n\n')
+    lines.append('If you want to edit an article, create a new issue or submit a pull request.')
+    lines.append('\n\n')
+    lines.append('Wiki generated for Dungeon {}.'.format(get_version_string()))
+    return lines
+
+
+def write_footer(path):
+    footer_format = 'Dungeon {}. Licensed under the BSD 3-Clause license.'
+    with open(os.path.join(path, get_footer_filename()), 'w') as footer_file:
+        footer_file.writelines(footer_format.format(get_version_string()))
+
+
 def main():
     dungeon_root = os.path.dirname(os.path.dirname(__file__))
+    wiki_root = os.path.join(dungeon_root, 'dungeon.wiki')
     clean_wiki_clone(dungeon_root)
+    get_version_string()
     wiki_json_path = os.path.join(dungeon_root, 'src', 'main', 'resources', 'wiki.json')
     with open(wiki_json_path) as wiki_file:
+        write_footer(wiki_root)
+        home_path = os.path.join(wiki_root, 'Home.md')
+        with open(home_path, 'w') as home_file:
+            home_file.writelines(get_home_markdown())
         wiki_json = json.load(wiki_file)
         for article in wiki_json['articles']:
             article_title = article['title']
             markdown = make_markdown_for_article(article)
-            file_path = os.path.join(dungeon_root, 'dungeon.wiki', make_filename_for_article(article_title))
+            file_path = os.path.join(wiki_root, make_filename_for_article(article_title))
             with open(file_path, 'w') as article_file:
                 article_file.writelines(markdown)
-                article_file.close()
 
 
 if __name__ == '__main__':
