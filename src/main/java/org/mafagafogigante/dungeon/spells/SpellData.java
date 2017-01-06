d0a64335573abe1b080978acbf09697a716f7a42
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-01-06 05:42:40 -0200

    Designed and implemented the Displace spell

--- src/main/java/org/mafagafogigante/dungeon/spells/SpellData.java
+++ src/main/java/org/mafagafogigante/dungeon/spells/SpellData.java
@@ -4,9 +4,14 @@ import org.mafagafogigante.dungeon.entity.creatures.Creature;
 import org.mafagafogigante.dungeon.entity.creatures.Hero;
 import org.mafagafogigante.dungeon.entity.creatures.HeroUtils;
 import org.mafagafogigante.dungeon.entity.items.Item;
+import org.mafagafogigante.dungeon.game.BlockedEntrances;
+import org.mafagafogigante.dungeon.game.Direction;
 import org.mafagafogigante.dungeon.game.DungeonString;
 import org.mafagafogigante.dungeon.game.Engine;
 import org.mafagafogigante.dungeon.game.Id;
+import org.mafagafogigante.dungeon.game.Location;
+import org.mafagafogigante.dungeon.game.Point;
+import org.mafagafogigante.dungeon.game.Random;
 import org.mafagafogigante.dungeon.io.Writer;
 import org.mafagafogigante.dungeon.stats.CauseOfDeath;
 import org.mafagafogigante.dungeon.stats.TypeOfCauseOfDeath;
@@ -177,6 +182,46 @@ public final class SpellData {
             Engine.rollDateAndRefresh(SECONDS_TO_CAST_UNVEIL);
             target.getLightSource().enable();
             Writer.write("You casted " + getName() + " on " + target.getName().getSingular() + ".");
+          }
+        }
+      }
+    });
+    putSpell(new Spell("DISPLACE", "Displace") {
+
+      static final int SECONDS_TO_CAST_DISPLACE = 15;
+      static final double DISPLACE_PROBABILITY = 0.5;
+
+      @Override
+      public void operate(Hero hero, String[] targetMatcher) {
+        if (targetMatcher.length == 0) {
+          Writer.write("Provide a target.");
+        } else {
+          Creature target = hero.findCreature(targetMatcher);
+          if (target != null) {
+            Engine.rollDateAndRefresh(SECONDS_TO_CAST_DISPLACE);
+            if (Random.roll(DISPLACE_PROBABILITY)) {
+              Location targetLocation = target.getLocation();
+              BlockedEntrances blockedEntrances = targetLocation.getBlockedEntrances();
+              List<Direction> unblockedDirections = new ArrayList<>();
+              for (Direction direction : Direction.values()) {
+                if (!blockedEntrances.isBlocked(direction)) {
+                  unblockedDirections.add(direction);
+                }
+              }
+              String prefix = "You casted " + getName() + " on " + target.getName().getSingular() + " successfully ";
+              if (unblockedDirections.isEmpty()) {
+                Writer.write(prefix + "but it could not be displaced as all directions are blocked.");
+              } else {
+                Direction direction = Random.select(unblockedDirections);
+                Point destinationPoint = new Point(targetLocation.getPoint(), direction);
+                Location destinationLocation = targetLocation.getWorld().getLocation(destinationPoint);
+                targetLocation.removeCreature(target);
+                destinationLocation.addCreature(target);
+                Writer.write(prefix + "and it was displaced " + direction.toString() + ".");
+              }
+            } else {
+              Writer.write("You failed to cast " + getName() + " on " + target.getName().getSingular() + ".");
+            }
           }
         }
       }
