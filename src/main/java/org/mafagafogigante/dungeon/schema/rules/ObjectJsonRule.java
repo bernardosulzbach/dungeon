6cbe1dacf5a40e6abf00a06c9d88b0557be2c531
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-02-16 15:43:13 -0200

    ObjectJsonRule should fail on unspecified fields
    Closed #350

--- src/main/java/org/mafagafogigante/dungeon/schema/rules/ObjectJsonRule.java
+++ src/main/java/org/mafagafogigante/dungeon/schema/rules/ObjectJsonRule.java
@@ -5,8 +5,10 @@ import org.mafagafogigante.dungeon.schema.JsonRule;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 class ObjectJsonRule implements JsonRule {
 
@@ -22,8 +24,13 @@ class ObjectJsonRule implements JsonRule {
       throw new IllegalArgumentException(value + " is not an object.");
     }
     JsonObject object = value.asObject();
+    Set<String> names = new HashSet<>(object.names());
     for (Entry<String, JsonRule> entry : rules.entrySet()) {
       entry.getValue().validate(object.get(entry.getKey()));
+      names.remove(entry.getKey());
+    }
+    if (!names.isEmpty()) {
+      throw new IllegalArgumentException(String.format("%s does not have a rule.", names.iterator().next()));
     }
   }
 
