3e684ef2f94404d90b4a8c5dc28e539ad0233c57
Vitalii Zymukha <unknownnpc@gmail.com>
2017-03-10 17:42:32 +0200

    Refactored the EnumJsonRule to reduce big-o complexity

--- src/main/java/org/mafagafogigante/dungeon/schema/rules/EnumJsonRule.java
+++ src/main/java/org/mafagafogigante/dungeon/schema/rules/EnumJsonRule.java
@@ -1,36 +1,28 @@
 package org.mafagafogigante.dungeon.schema.rules;
 
 import com.eclipsesource.json.JsonValue;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 class EnumJsonRule<T extends Enum<T>> extends StringJsonRule {
 
-  private final List<Enum<T>> enumValues = new ArrayList<>();
+  private final Set<String> enumValues = new HashSet<>();
 
   EnumJsonRule(Class<T> enumClass) {
-    Collections.addAll(enumValues, enumClass.getEnumConstants());
+    for (Enum<T> enumConstant : enumClass.getEnumConstants()) {
+      enumValues.add(enumConstant.toString());
+    }
   }
 
   @Override
   public void validate(JsonValue value) {
     super.validate(value);
     String valueAsString = value.asString();
-    if (!isValueExist(valueAsString)) {
+    boolean isValueExist = enumValues.contains(valueAsString);
+    if (!isValueExist) {
       throw new IllegalArgumentException(valueAsString + " is not exist in next enum values: " + enumValues + ".");
     }
-  }
-
-  private boolean isValueExist(String value) {
-    for (Enum<T> enumValue : enumValues) {
-      if (StringUtils.equals(enumValue.toString(), value)) {
-        return true;
-      }
-    }
-    return false;
   }
 
 }
