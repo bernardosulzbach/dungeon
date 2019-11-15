3bf88cbef6e52362f98f930ec5a78eda4f3f918e
Paul Luu <30512065+immutablebox@users.noreply.github.com>
2019-11-15 10:27:57 -0500

    Add map legend
    Retrieving adjacent locations relative to the player to display as a map legend.

--- src/main/java/org/mafagafogigante/dungeon/map/WorldMapWriter.java
+++ src/main/java/org/mafagafogigante/dungeon/map/WorldMapWriter.java
@@ -1,10 +1,17 @@
 package org.mafagafogigante.dungeon.map;
 
+import org.mafagafogigante.dungeon.game.Direction;
 import org.mafagafogigante.dungeon.game.DungeonString;
+import org.mafagafogigante.dungeon.game.Game;
+import org.mafagafogigante.dungeon.game.Location;
+import org.mafagafogigante.dungeon.game.Point;
 import org.mafagafogigante.dungeon.gui.GameWindow;
 import org.mafagafogigante.dungeon.io.Writer;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class WorldMapWriter {
 
@@ -36,13 +43,29 @@ public final class WorldMapWriter {
   private static void renderMap(@NotNull WorldMap map) {
     DungeonString string = new DungeonString();
     WorldMapSymbol[][] worldMapSymbolMatrix = map.getSymbolMatrix();
-    for (int i = 0; i < worldMapSymbolMatrix.length; i++) {
+    for (int i = 0; i < worldMapSymbolMatrix.length - 2; i++) {
       for (WorldMapSymbol symbol : worldMapSymbolMatrix[i]) {
         // OK as setColor verifies if the color change is necessary (does not replace a color by itself).
         string.setColor(symbol.getColor());
         string.append(symbol.getCharacterAsString());
       }
       if (i < worldMapSymbolMatrix.length - 1) {
+        string.append("\n");
+      }
+    }
+
+    //Rendering the map legend
+    ArrayList<Direction> directions = new ArrayList<>(Arrays.asList(Direction.values()));
+    directions.remove(Direction.UP);
+    directions.remove(Direction.DOWN);
+    for (int i = 0; i < directions.size(); i++) {
+      Point adjacentPoint = new Point(Game.getGameState().getHero().getLocation().getPoint(), directions.get(i));
+      if (Game.getGameState().getWorld().hasLocationAt(adjacentPoint)) {
+        Location adjacentLocation = Game.getGameState().getWorld().getLocation(adjacentPoint);
+        string.append("\t\t" + adjacentLocation.getName().getSingular().substring(0, 1) + " - " +
+                adjacentLocation.getName().getSingular());
+      }
+      if (i == 1) {
         string.append("\n");
       }
     }
